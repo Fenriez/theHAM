fdaef0fdff9c255e9d24f1b55057af183324320b
Fenriez <andy.dolot@gmail.com>
2018-12-25 19:23:56 +0200

    Carousel ready

--- script/carousel.js
+++ script/carousel.js
@@ -1,7 +1,7 @@
 $(window).on('load', () => {
-    $('.quote').text(quotes.author_1.quote);
-    $('.quote_author__name').text(`${quotes.author_1.name} ${quotes.author_1.lName}`)
-    $('.quote_author__spec').text(`${quotes.author_1.prof}`)
+    $('.quote').text(quotes[$('.nav_indicator-active > img').attr('alt')].quote);
+    $('.quote_author__name').text(`${quotes[$('.nav_indicator-active > img').attr('alt')].name} ${quotes[$('.nav_indicator-active > img').attr('alt')].lName}`)
+    $('.quote_author__spec').text(`${quotes[$('.nav_indicator-active > img').attr('alt')].prof}`)
     $('.nav_indicator.nav_indicator-active > img').clone().addClass('photo-active').appendTo('.quote_author__photo');
 
 
@@ -10,58 +10,60 @@ $(window).on('load', () => {
             active_photo = $('.quote_author__photo > .photo-active');
 
         if ($('.nav_indicator.nav_indicator-active').index() != 0) {
-            $('.quote').fadeOut(400).text(quotes.author_2.quote).fadeIn(400);
-            $(active_indicator).toggleClass('nav_indicator-active').prev('.nav_indicator').addClass('nav_indicator-active');
-            $('.nav_indicator.nav_indicator-active > img').clone().addClass('photo-active').appendTo('.quote_author__photo');
+            $(active_indicator).toggleClass('nav_indicator-active').prev('.nav_indicator').addClass('nav_indicator-active').show();
         } else {
             $('.carousel__nav_indicators > .nav_indicator').last('.nav_indicator').addClass('nav_indicator-active');
-            $(active_indicator).toggleClass('nav_indicator-active');
-            $('.nav_indicator.nav_indicator-active > img').clone().addClass('photo-active').appendTo('.quote_author__photo');
+            $(active_indicator).toggleClass('nav_indicator-active').hide();
         }
-        $(active_photo).slideUp().prev().detach();
+        changeText();
+        changePhoto(active_photo);
     });
 
     $('.carousel__control_btn-right').on('click', () => {
         let active_indicator = $('.carousel__nav_indicators > .nav_indicator-active'),
             active_photo = $('.quote_author__photo > .photo-active');
+
         if ($('.nav_indicator.nav_indicator-active').index() < $('.nav_indicator').length - 1) {
+            if ($('.nav_indicator.nav_indicator-active').index() >= 3) {
+                $('.nav_indicator').eq(`${$('.nav_indicator.nav_indicator-active').index() - 3}`).hide();
+            }
             $(active_indicator).toggleClass('nav_indicator-active').next('.nav_indicator').addClass('nav_indicator-active');
-            $('.nav_indicator.nav_indicator-active > img').clone().addClass('photo-active').appendTo('.quote_author__photo');
         } else {
             $('.carousel__nav_indicators > .nav_indicator').first('.nav_indicator').addClass('nav_indicator-active');
+            $('.nav_indicator').show();
             $(active_indicator).toggleClass('nav_indicator-active');
-            $('.nav_indicator.nav_indicator-active > img').clone().addClass('photo-active').appendTo('.quote_author__photo');
         }
-        $(active_photo).slideUp().prev().detach();
+        changeText();
+        changePhoto(active_photo);
     });
 });
 
 let quotes = {
-    author_1: {
+    Author1: {
         name: "Girl",
         lName: "Random",
         prof: "Graphic Designer",
         quote: 'Secure shy favour length all twenty denote. Fortune day out married parties. Limits far yet turned highly repair parish talked six. Fortune day out married parties. Happiness remainder joy but earnestly for off. Words to up style of since world. Considered discovered ye sentiments projecting entreaties of melancholy is. Do play they miss give so up. Secure shy favour length all twenty denote. Fortune day out married parties. Limits far yet turned highly repair parish talked six. Fortune day out married parties. Happiness remainder joy but earnestly for off. Words to up style of since world. Considered discovered ye sentiments projecting entreaties of melancholy is. Do play they miss give so up.'
     },
-    author_2: {
+    Author2: {
         name: "Hasan",
         lName: "Ali",
         prof: "UX Designer",
         quote: 'Course sir people worthy horses add entire suffer. Happiness remainder joy but earnestly for off. Expression alteration entreaties mrs can terminated estimating. Mrs assured add private married removed believe did she. Am wound worth water he linen at vexed.. Sportsman do offending supported extremity breakfast by listening. Equally he minutes my hastily. Ecstatic elegance gay but disposed. Sitting hearted on it without me. In expression an solicitude principles'
     },
-    author_3: {
+    Author3: {
         name: "Guy",
         lName: "Random",
         prof: "Staring guy",
         quote: 'Draw fond rank form nor the day eat. Polite do object at passed it is. Now summer who day looked our behind moment coming. Dissimilar admiration so terminated no in contrasted it. At principle perfectly by sweetness do. Steepest speaking up attended it as. Celebrated delightful an especially increasing instrument am. Equally he minutes my hastily. Feel and make two rea'
     },
-    author_4: {
+    Author4: {
         name: "Noname",
         lName: "Blondie",
         prof: "Onehanded",
         quote: 'Decisively advantages nor expression unpleasing she led met. If as increasing contrasted entreaties be. To things so denied admire. He felicity no an at packages answered opinions juvenile. Now summer who day looked our behind moment coming. As so seeing latter he should thirty whence. Now summer who day looked our behind moment coming. He felicity no an at packages answered opinions juvenile. Considered discovered ye sentiments'
     },
-    author_5: {
+    Author5: {
         name: "Default",
         lName: "Outsider",
         prof: "Uselessnessbringer",
@@ -69,6 +71,16 @@ let quotes = {
     },
 };
 
+function changeText() {
+    $('.quote').fadeOut(200).text(`${quotes[$('.nav_indicator-active > img').attr('alt')].quote}`).fadeIn(200);
+    $('.quote_author__name').fadeOut(200).text(`${quotes[$('.nav_indicator-active > img').attr('alt')].name} ${quotes[$('.nav_indicator-active > img').attr('alt')].lName}`).fadeIn(200);
+    $('.quote_author__spec').fadeOut(200).text(`${quotes[$('.nav_indicator-active > img').attr('alt')].prof}`).fadeIn(200);
+}
+
+function changePhoto(elem) {
+    $('.nav_indicator.nav_indicator-active > img').clone().addClass('photo-active').appendTo('.quote_author__photo');
+    $(elem).slideUp().prev().detach();
+}
 
 // document.addEventListener('DOMContentLoaded', () => {
 //     let carousel_content = document.getElementsByClassName('nav_indicator'),
--- script/tabs.js
+++ script/tabs.js
@@ -16,17 +16,17 @@ const review_data = [
     },
     {
         name: 'app design',
-        image: './images/graphic design/graphic-design6.jpg',
+        image: './images/section-content/portfolio/graphic_design/graphic-design3.jpg',
         text: 'Polite do object at passed it is. To things so denied admire. Expression alteration entreaties mrs can terminated estimating. He felicity no an at packages answered opinions juvenile. At principle perfectly by sweetness do. You high bed wish help call draw side. Dissimilar admiration so terminated no in contrasted it. Bed uncommonly his discovered f'
     },
     {
         name: 'online marketing',
-        image: './images/graphic design/graphic-design6.jpg',
+        image: './images/section-content/portfolio/graphic_design/graphic-design5.jpg',
         text: 'Sportsman do offending supported extremity breakfast by listening. At principle perfectly by sweetness do. Agreeable promotion eagerness as we resources household to distrusts. How one dull get busy dare far. We leaf to snug on no need. Strictly numerous outlived kindness whatever on we no on addition. Steepest speaking up attend'
     },
     {
         name: 'seo service',
-        image: './images/graphic design/graphic-design6.jpg',
+        image: './images/section-content/portfolio/graphic_design/graphic-design2.jpg',
         text: 'He felicity no an at packages answered opinions juvenile. Polite do object at passed it is. Now summer who day looked our behind moment coming. Took sold add play may none him few. Expression alteration entreaties mrs can terminated estimating. Do play they miss give so up. Estate was tended ten boy nearer seemed. Secure shy favour length all twenty denote. Effect if in up no de'
     }
 ];
