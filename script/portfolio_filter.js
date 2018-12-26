12f0928b59efe21e6ba19b26c91dc88a8ad46f21
Fenriez <andy.dolot@gmail.com>
2018-12-26 13:57:26 +0200

    Portfolio filter upgrading in progress

--- script/portfolio_filter.js
+++ script/portfolio_filter.js
@@ -1,16 +1,41 @@
-$('.portfolio_review__nav > .nav__btn').on('click', function () {
-    let data_group = $(this).text();
-    $('.portfolio_review__nav > .filtered').toggleClass('filtered');
-    $(this).toggleClass('filtered');
-    $.each($('.portfolio_image'), function (index, elem) {
-        if (data_group !== 'all') {
-            if ($(elem).data('group') !== data_group) {
-                $(elem).css('display', 'none');
-            } else {
-                $(elem).css('display', 'block');
-            }
-        } else {
-            $(elem).css('display', 'block');
-        };
+$(window).on('load', function() {
+    portfolioImagesLoader();
+
+    $('.portfolio_review__nav > .nav__btn').on('click', function () {
+        let data_group = $(this).text();
+        $('.portfolio_review__nav > .filtered').toggleClass('filtered');
+        $(this).toggleClass('filtered');
+        $.each($('.portfolio_image'), function (index, elem) {
+            if (data_group !== 'all') {
+                if ($(elem).data('group') !== data_group) {
+                    $(elem).hide();
+                } else {
+                    $(elem).show();
+                }
+            } else {                
+                portfolioImagesLoader();
+            };
+        });
+        console.log($('.portfolio_image:hidden').data('group') == data_group);
+        // if ($('.portfolio_image:hidden').length != 0 && $('.portfolio_image:hidden').data('group') == data_group || data_group == 'all') {
+        //     console.log(true);
+        // } 
+    });
+
+    $('.load_more_btn').on('click', function() {
+        $(this).hide().next().show();
+        let loading = setTimeout(() => {
+            $('.preloader').hide().prev().show();
+            clearTimeout('loading');
+        }, 4000);
     });
 });
+
+let portfolio_images = $('.portfolio_image');
+
+function portfolioImagesLoader() {
+    $(portfolio_images).hide();
+    for (let i = 0; i < 12; i++) {
+        $(portfolio_images[i]).show();
+    };
+}
